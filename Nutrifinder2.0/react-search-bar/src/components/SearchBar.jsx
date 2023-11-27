15be1fb623d25c0d411e5e0dbf25c56d0bd20681
sjesus <s.jesus10@yahoo.com>
2023-11-26 20:01:42 -0600

    Configured fetch calls to api

--- Nutrifinder2.0/react-search-bar/src/components/SearchBar.jsx
+++ Nutrifinder2.0/react-search-bar/src/components/SearchBar.jsx
@@ -1,11 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
 import { FaSearch } from "react-icons/fa";
+import "./SearchBar.css"
 
 export const SearchBar = () => {
+    const [input, setInput] = useState("");
+
+    const fetchData = (value) => {
+        fetch("https://api.nal.usda.gov/fdc/v1/foods/search?api_key=intnOzvjRdLQUdPgKcD3420RkD7pCogdZ3BYLwu6&query=" + value).then((response) => response.json()).then(json => {console.log(json);})
+    };
+
+    const handleChange = (value) => {
+        setInput(value);
+        fetchData(value);
+    };
+
     return <>
-    <div className="input-wrapper">
-    <FaSearch  id="search=icon" />
-    <input placeholder="Type to search..." />
-    </div>
+        <div className="input-wrapper">
+            <FaSearch  id="search=icon" />
+            <input
+                placeholder="Type to search..."
+                value= {input}
+                onChange={(e) => handleChange(e.target.value)}
+            />
+        </div>
     </>
 }
